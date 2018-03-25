43516638b322d86cd60284775ba41224a3b56302
fzlee <fzleee@gmail.com>
2018-03-25 15:19:45 +0800

    site ready

--- src/components/_articles/ArticlePreview.vue
+++ src/components/_articles/ArticlePreview.vue
@@ -13,7 +13,7 @@
         <h2><router-link :to="`/articles/${article.url}`">{{article.title}}</router-link></h2>
         <hr>
         <p v-if="!article.need_key">{{article.preview}}...</p>
-        <p v-else class="text-danger"><i>请输入密码访问</i></p>
+        <p v-else class="text-danger"><i>文章已被加密，请输入密码访问</i></p>
         <div class="float-right"><router-link :to="`/articles/${article.url}`">阅读全文&gt;&gt;&gt;</router-link></div>
       </div>
     </div>
