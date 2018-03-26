53972da0eb6dde5dd0ce9dfb9b13273710708c4e
fzlee <fzleee@gmail.com>
2018-03-26 11:38:29 +0800

    use analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import VueAnalytics from 'vue-analytics'
 import Router from 'vue-router'
 import App from './App.vue'
 import http from '@/services/http'
@@ -7,6 +8,10 @@ import router from '@/router/index'
 
 Vue.use(BootstrapVue)
 Vue.use(Router)
+Vue.use(VueAnalytics, {
+  id: ['UA-39931077-2']，
+  router
+})
 
 Vue.prototype.$http = http
 Vue.config.productionTip = false
